32fbfeaea4a80287cca59f643cea0159e0997379
Estervelle Bennett <bennettestervelle@gmail.com>
2025-06-26 16:38:20 -0400

    Added processor code, modified requirements and dockerfile

---
+++ batch_processor/processor.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from typing import List, Dict, Tuple
+from kafka import KafkaProducer
+import json
+import os
+
+
+producer = KafkaProducer(
+    bootstrap_server=['localhost:9092'],
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+)
+
+
+
+data = "data/financial_transactions.csv"
+
+with open(data, 'r') as file:
+    for line in file:
+        producer.send('transactions', line.encode('utf-8'))
+producer.flush()
+
+
+
