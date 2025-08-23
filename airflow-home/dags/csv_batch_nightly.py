0939e9b4f5459ce9c223919c8213b005a4661d88
Estervelle Bennett <bennettestervelle@gmail.com>
2025-08-23 15:43:41 -0400

    Added alerts and email for airflow

--- airflow-home/dags/csv_batch_nightly.py
+++ airflow-home/dags/csv_batch_nightly.py
@@ -3,9 +3,24 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.providers.docker.operators.docker import DockerOperator
 
+
+
+default_args = {
+    'owner': 'E.B.',
+    'depends_on_past': False,
+    'start_date': datetime(2025, 8, 1),
+    'email': ['info@pubmerge.com'],  # Who gets alerts
+    'email_on_failure': True,          # Email if task fails
+    'email_on_retry': False,           # Email on retries (usually annoying)
+    'email_on_success': False,         # Email on success (optional)
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5)
+}
+
 with DAG(
     dag_id="csv_batch_nightly",
     schedule="0 0 * * *",           # 12:00 AM nightly
+    default_args=default_args,
     start_date=datetime(2025, 8, 1),
     catchup=False,
     dagrun_timeout=timedelta(hours=2),
